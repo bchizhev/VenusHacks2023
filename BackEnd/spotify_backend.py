7eb41956e3ae708611c4d11c2f5e888d22aa571d
Brianna Chizhevsky <bchizhev@uci.edu>
2023-05-27 15:29:21 -0700

    BRO

--- BackEnd/spotify_backend.py
+++ BackEnd/spotify_backend.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from flask import Flask, redirect, request
 import requests
 import urllib
+import json
 
 app = Flask(__name__)
 load_dotenv()
@@ -32,6 +33,7 @@ def login():
         'scope': 'user-read-private user-read-email',
     }
     authorize_url = f'{AUTHORIZE_URL}?{urllib.parse.urlencode(params)}'
+    # print(authorize_url.access_token)
     return redirect(authorize_url)
 
 @app.route('/callback')
@@ -47,7 +49,48 @@ def callback():
     response = requests.post(TOKEN_URL, data=data)
     # Handle the response and retrieve the access token
     # You can save the access token in a session or a database for further use
-    return response.text
+    resp = response.json()
+
+    print(resp['access_token'])
+    return redirect('http://localhost:5500/FrontEnd/?token=' + resp['access_token'])
+
+@app.route('/topTenTracks', methods=['POST'])
+def get_top_10_tracks():
+    token = json.loads(request.data)['authToken']
+    auth_header = {'Authorization': 'Bearer ' + token}
+    # modify me in url to get user's spotify information
+    url = 'https://api.spotify.com/v1/me/top/tracks'
+    msg = '?time_range=medium_term&limit=10&offset=5'
+    try: 
+        result = requests.get(url,
+                            #    params = {
+                            #        'time_range': 'medium_term',
+                            #        'limit': 10,
+                            #        'offset': 5
+                            #    },
+                          headers=auth_header)
+    except e:
+        print(e)
+    print(result.data)
+    return ""
+    # json_result = json.loads(result.content)
+    # print(json_result)
+
+    """if result.status_code == 200:
+        data = result.json()
+        # Process the data as needed
+        print(data)
+    else:
+        print('Error:', result.status_code)"""
+    """auth_header = {'Authorization': 'Bearer ' + token}
+    # modify me to get user's spotify information
+    url = 'https://api.spotify.com/v1/me/top/tracks'
+    # query = f'?offset=1&limit=10'
+    query_url = url
+    result = requests.get(query_url, headers = auth_header)
+    json_result = json.loads(result.content)
+    print(json_result)"""
+
 
 if __name__ == '__main__':
     app.run(port=8000)
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,23 +1,26 @@
-let serverUrl = 'https://localhost:8000/callback';
+let serverUrl = 'http://localhost:8000/topTenTracks';
 
 
 function submit() {
-    let answer1 = document.getElementById('question1').value;
+    // let answer1 = document.getElementById('question1').value;
 
 
+    const urlParams = new URLSearchParams(window.location.search);
+    const authToken = urlParams.get('token');
 
+    console.log('this is the token from the url', authToken)
 
- 
     fetch(serverUrl, {
-      mathod:'POST',
+      method:'POST',
       body: JSON.stringify({
-        mood: answer1,
-        erhrtj: 23,
-        authToken: 'dfghdghr345tg'
+        // mood: answer1,
+        authToken: authToken
        
       })
     })
-        .then(res => res.json())
-        .then(data => {
-            document.getElementById('display').innerHTML = data.message
-        })
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById('display').innerHTML = data.message
+    });
+
+}
