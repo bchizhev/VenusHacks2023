fddb5772fde49df4960a66299dc4106a9faa7795
Veronic Trinh <veroniqt@uci.edu>
2023-05-27 21:17:19 -0700

    Creating functionality to get similar artists to a user's top artists and then getting top track from similar artist

--- BackEnd/spotify_backend.py
+++ BackEnd/spotify_backend.py
@@ -18,7 +18,6 @@ TOKEN_URL = 'https://accounts.spotify.com/api/token'
 
 # Redirect URI that Spotify will redirect back to after authentication
 REDIRECT_URI = 'http://localhost:8000/callback'
-# REDIRECT_URI = 'http://localhost:5000'
 
 @app.route('/')
 def index():
@@ -33,7 +32,6 @@ def login():
         'scope': 'user-read-private user-read-email user-top-read playlist-modify-private',
     }
     authorize_url = f'{AUTHORIZE_URL}?{urllib.parse.urlencode(params)}'
-    # print(authorize_url.access_token)
     return redirect(authorize_url)
 
 @app.route('/callback')
@@ -47,8 +45,6 @@ def callback():
         'client_secret': CLIENT_SECRET,
     }
     response = requests.post(TOKEN_URL, data=data)
-    # Handle the response and retrieve the access token
-    # You can save the access token in a session or a database for further use
     resp = response.json()
 
     print(resp['access_token'])
@@ -59,24 +55,15 @@ def get_top_10_tracks():
     token = json.loads(request.data)['authToken']
     auth_header = {'Authorization': 'Bearer ' + token}
     url = 'https://api.spotify.com/v1/me/top/artists'
-    try: 
-        result = requests.get(url, headers=auth_header)
-    except e:
-        print(e)
-    """print(result)
-    print('RESULTS:',len(result.json()['items']))
-    print('RESULTS:', result.json()['items'][0])
-    print('RESULTS:', result.json()['items'][0]['name'])
-    print('RESULTS:', result.json()['items'][0]['id'])"""
-    # print(result.json())
+    result = requests.get(url, headers=auth_header)
+
     artists_info = extract_artist_info(result)
     user_id = get_user_spotify_id(auth_header)
-    # similar_artist = get_similar_artists()
-    playlist_id, playlist_uri = create_new_playlist(user_id, token)
+    similar_songs = get_similar_artists_top_songs(artists_info, auth_header)
+    # playlist_id, playlist_uri = create_new_playlist(user_id, token)
+    # print(result.json()['items'][0])
     # artists_info['Stray Kids']
     return jsonify(result.json())
-    # json_result = json.loads(result.content)
-    # print(json_result)
 
 def extract_artist_info(result) -> dict:
     artist_info = {}
@@ -85,8 +72,12 @@ def extract_artist_info(result) -> dict:
         info_dict['id'] = result.json()['items'][index]['id']
         info_dict['genres'] = result.json()['items'][index]['genres']
         artist_info[result.json()['items'][index]['name']] = info_dict
-        # print(artist_info)
     return artist_info
+
+def filter_artists(mood_keyword, artists_info):
+    for artist, info in artists_info.items():
+        if mood_keyword not in info['genres']:
+            del artists_info[artist]
 
 def get_user_spotify_id(auth_header):
     url = 'https://api.spotify.com/v1/me'
@@ -100,19 +91,45 @@ def create_new_playlist(user_id, token) -> tuple:
         'Content-Type': 'application/json'
     }
     data = json.dumps({
-        'name': 'mood~~id + uri',
+        'name': '~~mood~~',
         'description': 'A new curated playlist unique for you based on your mood!',
         'public': False
     })
     result = requests.post(url, headers=auth_header, data=data)
-    print(result.json())
     playlist_id = result.json()['id']
     playlist_uri = result.json()['uri']
     return playlist_id, playlist_uri
 
+def get_similar_artists_top_songs(artists, auth_header):
+    song_info = []
+    print(artists)
+    for artist, info in artists.items():
+        # print(info['id'])
+        id = info['id']
+        # print(artist, info['id'])
+        url = f'https://api.spotify.com/v1/artists/{id}/related-artists'
+        result = requests.get(url, headers=auth_header)
+        # print(result)
+        five_similar_artists = result.json()['artists'][:4]
+        for artists in five_similar_artists:
+            artist_id = artists['id']
+            url = f'https://api.spotify.com/v1/artists/{artist_id}/top-tracks?market=US'
+            result = requests.get(url, headers=auth_header)
+            # print(artist)
+            
+            print('Artist name:', artists['name'])
+            print('Artist id:', artist_id)
+            print('Song:', result.json()['tracks'][0]['name'])
+            print('Song uri:', result.json()['tracks'][0]['uri'])
+            print()
+            song_uri = result.json()['tracks'][0]['uri']
+            if song_uri not in song_info:
+                song_info.append(song_uri)
+    return song_info
 
-def get_similar_artists(artist, auth_header):
-    pass
+def get_artist_top_song(artist_id, auth_header):
+    url = f'https://api.spotify.com/v1/artists/{artist_id}/top-tracks'
+
 
 
 if __name__ == '__main__':
